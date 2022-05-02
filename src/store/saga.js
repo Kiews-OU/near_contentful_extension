95e83f0d43d8ec42a2733b582d5684cc8e2ae805
armen95 <armmarj1006@gmail.com>
2022-05-02 18:21:08 +0400

    fixed extension

--- src/store/saga.js
+++ src/store/saga.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { put, takeEvery } from "redux-saga/effects";
 import { getPrice } from "./wallet/action";
 function* GetNearPrice() {
-    let result = yield axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`)
+    let result = yield axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
     // console.log(result.data);
     let nearFound = result.data.find((el) => el.id === "near");
+    // console.log(nearFound);
     yield put (getPrice(nearFound.current_price))
 }
 
--- src/store/wallet/reducer.js
+++ src/store/wallet/reducer.js
@@ -1,38 +1,38 @@
-let walletState = {
-  walletID: "",
+export let walletState = {
+  ID: "",
   walletSum: "",
   sum: "",
   rid: "",
   transaction: [],
   price: "",
   CID: "",
-  id:"",
-  key:""
 };
 export const walletReducer = (state = walletState, option) => {
   switch (option.type) {
-    case "setdata":
-      state.walletID = option.id
-      state.walletSum = option.available
+    case "setid":
+      state.ID = option.value
       return state
-    case "enterval":
-      state[option.key] = option.data
-      return state
-    case "transaction":
-      state.transaction.push(option.data)
-      return state
-    case "isError":
-      state.TrError = option.error
-      return state
-    case "getPrice":
-      state.price = option.payload
-      return state
-    case "setContr":
-      state.CID = option.value
-      return state
-      case "setAccData":
-        state[option.key]=option.value
+      case "setdata":
+        state.walletSum = option.available
         return state
+      case "enterval":
+        state[option.key] = option.data
+        return state
+      case "transaction":
+        state.transaction.push(option.data)
+        return state
+      case "isError":
+        state.TrError = option.error
+        return state
+      case "getPrice":
+        state.price = option.payload
+        return state
+      case "setContr":
+        state.CID = option.value
+        return state
+        case "setAccData":
+          state[option.key]=option.value
+          return state
     default:
       return state;
   }
